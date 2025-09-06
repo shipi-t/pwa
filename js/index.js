77d625764699f41d04f69c7403ef90983ac84488
shipi-t <shqipron.t@gmail.com>
2025-09-06 15:11:56 +0200

    added icon in manifest

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-        .register("../service-worker.js")
+        .register("./service-worker.js")
         .then(() => console.log("Service Worker registered"))
         .catch((err) => console.error("Service Worker registration failed:", err));
 }
